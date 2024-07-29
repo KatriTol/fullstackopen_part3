b3c90b3997f01eaf147c0e6672e0445a036eebcf
Katri Tolonen <katritolonen2@kamk.fi>
2024-07-29 22:53:29 +0300

    3.20

--- models/person.js
+++ models/person.js
@@ -22,7 +22,16 @@ const personSchema = new mongoose.Schema({
       },
     number: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            // validointifunktio:
+            validator: function(v) {
+              // 2-3 merkkiä + väliviiva+ vähintään 5 merkkiä
+              return /^\d{2,3}-\d{5,}$/.test(v);
+            },
+            
+            message: props => `${props.value} is not a valid phone number!`
+          }
       },
   })
 
